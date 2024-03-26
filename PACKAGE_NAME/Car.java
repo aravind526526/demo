dae8a2b77317f9efea6f11c88bff328cf3fed476
aravind <chepyalaaravind5262gmail.com>
2024-03-25 20:07:00 -0500

    added car method

---
+++ PACKAGE_NAME/Car.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+import java.awt.desktop.SystemEventListener;
+
+public class Car {
+    SystemEventListener s=new SystemEventListener() {
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    };
+}
